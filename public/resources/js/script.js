b17ba5527a3e50ff2c631797a238d78d6fb4b5c8
Geek-lock <jweffergerman@gmail.com>
2023-10-12 14:19:44 -0300

    Validacion en campo de suscripción

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -1,3 +1,12 @@
-document.getElementById("suscripcion").addEventListener("click", function() {
-    alert("¡Suscripción con éxito!");
-});
+
+function validarEmail() {
+    const emailInput = document.getElementById('emailInput');
+    const email = emailInput.value;
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+  
+    if (emailRegex.test(email)) {
+      alert('¡Correo electrónico válido, Suscripción completa!');
+    } else {
+      alert('Correo electrónico no válido');
+    }
+  }
